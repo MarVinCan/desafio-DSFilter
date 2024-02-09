78b1e316a596da0bacb3c37d4cab66dc5c45459e
Marcus Vinicius <marcus.v.e.candido@gmail.com>
2024-02-09 17:00:11 -0300

    Header created

---
+++ src/componets/Header/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+
+export default function Header(){
+    return(
+        <header>
+            <div className='dsf-container dsf-items-header'>
+                <h1>DSFilter</h1>
+                <div className='dsf-number-products-header'><h2>6 producto(s)</h2></div>
+            </div>
+        </header>
+    );
+}
